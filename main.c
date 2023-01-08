6a314caaa27b00b2831d9cc4d449abc600f8a932
Gabriela Ochoa Dąderska <108947060+GabrielaOchoaDaderska@users.noreply.github.com>
2023-01-08 17:34:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,140 +1,129 @@
 
-#include<gtk/gtk.h>
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<dirent.h>
-//
-GtkWidget* window;
-GtkWidget* list;
-GtkWidget* directory;
-GtkWidget* files;
-GtkWidget* scrolled_window;
-GtkWidget* scrolled_window2;
-GtkWidget* scrolled_window3;
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 
-void openfile(GtkWidget* widget, gpointer* data)
-{
-	gchar* string;
-	GtkTreeIter iter;
-	GtkTreeModel* model;
-	if(gtk_tree_selection_get_selected(GTK_TREE_SELECTION(data), &model, &iter) == TRUE)
-	{
-		gtk_tree_model_get(model, &iter, 0, &string, -1);
-		if(strcmp(string, "..")!=0){
-			gtk_list_store_clear(GTK_LIST_STORE(files));
-			DIR* dir;
-			struct dirent* ent;
-			if((dir=opendir(string))!=NULL){
-				while((ent=readdir(dir))!=NULL){
-					GtkTreeIter iter;
-					gtk_list_store_append(GTK_LIST_STORE(files), &iter);
-					gtk_list_store_set(GTK_LIST_STORE(files), &iter, 0, ent->d_name, -1);
-				}
-				closedir(dir);
-			}
-			else{
-				g_print("Nie można otworzyć folderu %s\n", string);
-			}
-			gtk_entry_set_text(GTK_ENTRY(directory), string);
-		}
-		else{
-			gchar* old_dir;
-			old_dir=strdup(string);
-			gtk_list_store_clear(GTK_LIST_STORE(files));
-			DIR* dir;
-			struct dirent* ent;
-			if((dir=opendir(old_dir))!=NULL){
-				while((ent=readdir(dir))!=NULL){
-					GtkTreeIter iter;
-					gtk_list_store_append(GTK_LIST_STORE(files), &iter);
-					gtk_list_store_set(GTK_LIST_STORE(files), &iter, 0, ent->d_name, -1);
-				}
-				closedir(dir);
-			}
-			else{
-				g_print("Nie można otworzyć folderu %s\n", old_dir);
-			}
-			gtk_entry_set_text(GTK_ENTRY(directory), old_dir);
-			free(old_dir);
-		}
-		g_free(string);
+GtkWidget *file_list;
+
+//Funkcja do wyświetlania informacji o plikach
+void show_files_info(const char *dirname) {
+	DIR *dir;
+	struct dirent *entry;
+	if ((dir = opendir(dirname)) == NULL) {
+		perror("opendir() error");
+		return;
 	}
+	while ((entry = readdir(dir)) != NULL) {
+		printf("Name: %s\n", entry->d_name);
+		printf("Inode: %ld\n", entry->d_ino);
+		printf("Reclen: %d\n", entry->d_reclen);
+		printf("Type: %d\n", entry->d_type);
+		printf("\n");
+	}
+	closedir(dir);
 }
 
-int main (int argc, char* argv[])
-{
-	gtk_init(&argc, &argv);
+//Funkcja do wyświetlania zawartości katalogu
+void show_files(const char *dirname) {
+	DIR *dir;
+	struct dirent *entry;
+	if ((dir = opendir(dirname)) == NULL) {
+		perror("opendir() error");
+		return;
+	}
+	while ((entry = readdir(dir)) != NULL) {
+		GtkTreeIter iter;
+		gtk_list_store_append(GTK_LIST_STORE(file_list), &iter);
+		gtk_list_store_set(
+			GTK_LIST_STORE(file_list), &iter,
+			0, entry->d_name,
+			-1
+		);
+	}
+	closedir(dir);
+}
 
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "Przeglądarka Plików");
-	gtk_window_set_default_size(GTK_WINDOW(window), 700, 500);
-	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+//Funkcja do wyboru folderu
+static void select_folder_cb(GtkWidget *widget, gpointer data) {
+	GtkWidget *dialog;
+	dialog = gtk_file_chooser_dialog_new("Select folder", NULL, 
+		GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
+		"_Cancel", GTK_RESPONSE_CANCEL,
+		"_Open", GTK_RESPONSE_ACCEPT,
+		NULL);
+	if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+		char *filename;
+		filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+		gtk_list_store_clear(GTK_LIST_STORE(file_list));
+		show_files(filename);
+		show_files_info(filename);
+		g_free(filename);
+	}
+	gtk_widget_destroy(dialog);
+}
 
-	GtkWidget* vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-	gtk_container_add(GTK_CONTAINER(window), vbox);
+static void activate(GtkApplication *app, gpointer user_data) {
+	GtkWidget *window;
+	GtkWidget *grid;
+	GtkWidget *scrolled_window;
+	GtkWidget *select_folder;
+	GtkCellRenderer *renderer;
+	GtkTreeViewColumn *column;
 
-	GtkWidget* hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-	gtk_container_add(GTK_CONTAINER(vbox), hbox);
+	//Tworzenie okna i siatki
+	window = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "Przeglądarka plików");
+	gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
+	grid = gtk_grid_new();
+	gtk_container_add(GTK_CONTAINER(window), grid);
+
+	//Tworzenie paska bocznego
+	GtkWidget *side_bar;
+	side_bar = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	gtk_grid_attach(GTK_GRID(grid), side_bar, 0, 1, 1, 3);
+
+	GtkWidget *home_button;
+	home_button = gtk_button_new_with_label("Home");
+	gtk_box_pack_start(GTK_BOX(side_bar), home_button, FALSE, FALSE, 0);
+
+	GtkWidget *documents_button;
+	documents_button = gtk_button_new_with_label("Documents");
+	gtk_box_pack_start(GTK_BOX(side_bar), documents_button, FALSE, FALSE, 0);
+
+	GtkWidget *downloads_button;
+	downloads_button = gtk_button_new_with_label("Downloads");
+	gtk_box_pack_start(GTK_BOX(side_bar), downloads_button, FALSE, FALSE, 0);
+
+	//Tworzenie listy plików
 	scrolled_window = gtk_scrolled_window_new(NULL, NULL);
-	gtk_container_set_border_width(GTK_CONTAINER(scrolled_window), 10);
+	gtk_grid_attach(GTK_GRID(grid), scrolled_window, 1, 1, 1, 3);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_widget_set_size_request(scrolled_window, 200, 400);
-	gtk_box_pack_start(GTK_BOX(hbox), scrolled_window, FALSE, FALSE, 0);
 
-	list = gtk_list_store_new(1, G_TYPE_STRING);
-	GtkTreeIter iter;
+	file_list = gtk_list_store_new(1, G_TYPE_STRING);
+	GtkWidget *file_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(file_list));
+	gtk_container_add(GTK_CONTAINER(scrolled_window), file_view);
 
-	gtk_list_store_append(GTK_LIST_STORE(list), &iter);
-	gtk_list_store_set(GTK_LIST_STORE(list), &iter, 0, "/", -1);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes("Nazwa pliku", renderer, "text", 0, NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(file_view), column);
 
-	GtkTreeModel* model = GTK_TREE_MODEL(list);
-	GtkWidget* tree_view = gtk_tree_view_new_with_model(model);
-	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(tree_view), FALSE);
-
-	GtkCellRenderer* renderer = gtk_cell_renderer_text_new();
-	GtkTreeViewColumn* column = gtk_tree_view_column_new_with_attributes("Folder", renderer, "text", 0, NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
-
-	GtkTreeSelection* selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(tree_view));
-	gtk_container_add(GTK_CONTAINER(scrolled_window), tree_view);
-
-	scrolled_window2 = gtk_scrolled_window_new(NULL, NULL);
-	gtk_container_set_border_width(GTK_CONTAINER(scrolled_window2), 10);
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window2), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_widget_set_size_request(scrolled_window2, 200, 400);
-	gtk_box_pack_start(GTK_BOX(hbox), scrolled_window2, FALSE, FALSE, 0);
-
-	files = gtk_list_store_new(1, G_TYPE_STRING);
-
-	GtkTreeModel* model2 = GTK_TREE_MODEL(files);
-	GtkWidget* tree_view2 = gtk_tree_view_new_with_model(model2);
-	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(tree_view2), FALSE);
-
-	GtkCellRenderer* renderer2 = gtk_cell_renderer_text_new();
-	GtkTreeViewColumn* column2 = gtk_tree_view_column_new_with_attributes("Plik", renderer2, "text", 0, NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view2), column2);
-
-	GtkTreeSelection* selection2 = gtk_tree_view_get_selection(GTK_TREE_VIEW(tree_view2));
-	gtk_container_add(GTK_CONTAINER(scrolled_window2), tree_view2);
-
-	scrolled_window3 = gtk_scrolled_window_new(NULL, NULL);
-	gtk_container_set_border_width(GTK_CONTAINER(scrolled_window3), 10);
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window3), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_widget_set_size_request(scrolled_window3, 200, 400);
-	gtk_box_pack_start(GTK_BOX(hbox), scrolled_window3, TRUE, TRUE, 0);
-
-	directory = gtk_entry_new();
-	gtk_container_add(GTK_CONTAINER(scrolled_window3), directory);
-
+	select_folder = gtk_button_new_with_label("Wybierz folder");
+	gtk_grid_attach(GTK_GRID(grid), select_folder, 0, 0, 1, 1);
+	g_signal_connect(G_OBJECT(select_folder), "clicked", G_CALLBACK(select_folder_cb), NULL);
 	gtk_widget_show_all(window);
+}
 
-	g_signal_connect(selection, "changed", G_CALLBACK(openfile), selection);
+int main(int argc, char **argv) {
+	GtkApplication *app;
+	int status;
 
-	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+	status = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
 
-	gtk_main();
-
-	return 0;
+	return status;
 }
